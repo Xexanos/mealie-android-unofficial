a3d969f59874bbf5340d980d345fea0ce9ba4be3
Lars Winterhalder <lars.winterhalder@conciso.de>
2026-05-25 10:22:26 +0200

    commit changes missed during review

--- app/src/main/java/dev/xexanos/mealie/MainActivity.kt
+++ app/src/main/java/dev/xexanos/mealie/MainActivity.kt
@@ -3,6 +3,10 @@ package dev.xexanos.mealie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dev.xexanos.mealie.core.ui.theme.MealieTheme
 import dev.xexanos.mealie.navigation.AppNavGraph
@@ -10,9 +14,12 @@ import dev.xexanos.mealie.navigation.AppNavGraph
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MealieTheme {
-                AppNavGraph(navController = rememberNavController())
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppNavGraph(navController = rememberNavController())
+                }
             }
         }
     }
