b7552f4574d217834f04461c0c875b34575e98cf
Lars Winterhalder <lars.winterhalder@conciso.de>
2026-05-26 15:46:55 +0200

    Add testTag constants to ServerUrlScreen for E2E test access

---
+++ feature/auth/src/main/java/dev/xexanos/mealie/feature/auth/ui/ServerUrlTestTags.kt
@@ -0,0 +1,8 @@
+package dev.xexanos.mealie.feature.auth.ui
+
+object ServerUrlTestTags {
+    const val URL_TEXT_FIELD = "serverUrl_urlTextField"
+    const val CONNECT_BUTTON = "serverUrl_connectButton"
+    const val ERROR_TEXT = "serverUrl_errorText"
+    const val PROGRESS_INDICATOR = "serverUrl_progressIndicator"
+}
